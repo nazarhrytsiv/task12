9cec887d69c202ea9416ce9302c3e076d3306402
Nazar <nazar.hrytsiv7@gmail.com>
2018-04-03 14:16:08 +0300

    Create a Rational number

---
+++ Project1/Main.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MathSuite.Core.Numeric
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Rational> array = new List<Rational>();
+            string numerator;
+            string denominator;
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("{0} Number", i + 1);
+                numerator = Console.ReadLine();
+                denominator = Console.ReadLine();
+                array.Add(new Rational(Convert.ToInt32(numerator), Convert.ToInt32(denominator)));
+            }
+            Rational result = array[0];
+            for (int i = 1; i < array.Count; i++)
+            {
+                result += array[i];
+            }
+            Console.WriteLine(result.Print());
+
+            result = array[0];
+
+            for (int i = 1; i < array.Count; i++)
+            {
+                result *= array[i];
+            }
+            Console.WriteLine(result.Print());
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Project1/Rational.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+
+namespace MathSuite.Core.Numeric
+{
+
+    public class Rational
+    {
+        #region Static fields
+        private const string NAN_LITERAL = "NaN";
+        #endregion
+
+        //Use Hungary anotation
+        private readonly int _numerator, _denominator;
+
+        #region Consturctors
+        public Rational(int numerator) : this(numerator, 1) { }
+
+        public Rational(int numerator, int denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new ArgumentException(NAN_LITERAL);
+            }
+
+            this._numerator = numerator;
+            this._denominator = denominator;
+
+            if (IsReduce())
+            {
+                int reduce_number = GCD();
+
+                this._numerator /= reduce_number;
+                this._denominator /= reduce_number;
+            }
+        }
+        #endregion
+
+        private bool IsReduce()
+        {
+            bool answer = false;
+            if (GCD() > 1)
+            {
+                answer = true;
+                return answer;
+            }
+            return answer;
+        }
+
+        private int GCD()
+        {
+            int answer = 1;
+            int smallest = Math.Min(this._numerator, this._denominator);
+            for (int i = smallest; i >= 2; i--)
+            {
+                if (this._numerator % i == 0 & _denominator % i == 0)
+                {
+                    answer = i;
+                    return answer;
+                }
+            }
+            return answer;
+        }
+
+        public string Print()
+        {
+            return _numerator.ToString() + "/" + _denominator.ToString();
+        }
+
+        public static Rational operator +(Rational first_number, Rational second_number)
+        {
+            if (first_number._denominator == second_number._denominator)
+            {
+                return new Rational(first_number._numerator + second_number._numerator, first_number._denominator);
+            }
+            else
+            {
+                return new Rational(first_number._numerator * second_number._denominator + second_number._numerator * first_number._denominator,
+                    first_number._denominator * second_number._denominator);
+            }
+        }
+
+        public static Rational operator -(Rational first_number, Rational second_number)
+        {
+            if (first_number._denominator == second_number._denominator)
+            {
+                return new Rational(first_number._numerator - second_number._numerator, first_number._denominator);
+            }
+            else
+            {
+                return new Rational(first_number._numerator * second_number._denominator - second_number._numerator * first_number._denominator,
+                    first_number._denominator * second_number._denominator);
+            }
+        }
+
+        public static Rational operator *(Rational first_number, Rational second_number)
+        {
+            return new Rational(first_number._numerator * second_number._numerator, first_number._denominator * second_number._denominator);
+        }
+
+        public static Rational operator /(Rational first_number, Rational second_number)
+        {
+            return new Rational(first_number._numerator * second_number._denominator, first_number._denominator * second_number._numerator);
+        }
+    }
+
+}
+    
